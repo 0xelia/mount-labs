e20eb90f27c2a09bb39e5cca6dc0d9f8c185c384
0xelia <elia.vanon@yahoo.com>
2023-02-07 08:49:43 +0100

    first commit

---
+++ components/heroHome.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="h-screen bg-slate-200 relative flex justify-center items-center overflow-hidden">
+        <img src="../assets/img/bg.png" alt="" class="absolute w-screen z-[1]">
+        <img src="../assets/img/montain-lx.png" alt="" class="absolute w-screen z-[3]">
+        <img src="../assets/img/montain-rx.png" alt="" class="absolute w-screen z-[4] ">
+        <img src="../assets/img/man.png" alt="" class="absolute w-screen  z-[5]">
+        <div class="container text-center">
+            <h1 class="text-slate-100 relative z-[2] -translate-y-3 font-bold text-[270px] leading-[300px] " >Mount</h1>
+            <h1 class="text-slate-100 relative z-[2] -translate-y-3 font-bold text-[270px] leading-[300px] " >Labs</h1>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    *{
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;800&display=swap');
+        font-family: 'Montserrat';
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <HeroHome />
+        <section id="sec_2" class="h-screen flex justify-center items-center">
+            <h1>Section 2</h1>
+        </section>
+    </div>
+</template>
+
+<script>
+import HeroHome from '~~/components/heroHome.vue';
+
+export default {
+    components: {HeroHome}
+}
+</script>
+
+<style lang="scss" scoped>
+#sec_2{
+    position: relative;
+    // &::after{
+    //     content: '';
+    //     display: block;
+    //     position: absolute;
+    //     width: 100%;
+    //     height: 170px;
+    //     background: linear-gradient(0deg, rgb(255, 255, 255) 0%, rgba(255,255,255,0) 100%);;
+    //     top: -170px;
+    //     z-index: 9999999;
+    // }
+}
+</style>
